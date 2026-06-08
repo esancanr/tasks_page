21a8d2a68f509264783698ccdaa0a0ae22964d7d
rmiguel <e.eduardo.s234@gmail.com>
2026-06-08 10:32:09 -0500

    Updating task model

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,7 +1,29 @@
-from pydantic import BaseModel
+from typing import Optional
+from pydantic import BaseModel, Field
+from bson import ObjectId
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid ObjectId')
+        return str(v)
+
 
 class Task(BaseModel):
-    id : str
-    tittle : str
-    description : str
-    completed : bool = False
+    id: Optional[PyObjectId] = Field(alias='_id')
+    title: str
+    description: str
+    completed: bool = False
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        json_encoders = {
+            ObjectId: str
+        }
